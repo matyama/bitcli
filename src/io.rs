a5a86b9324507e9ca95e22ef6d544263ecdfc48e
Martin Matyášek <martin.matyasek@gmail.com>
2024-08-07 22:00:53 +0200

    test(io): add tests for reading entries from stdin

--- src/io.rs
+++ src/io.rs
@@ -5,7 +5,7 @@ use std::str::FromStr;
 
 use async_stream::try_stream;
 use futures_util::TryStream;
-use tokio::io::{self, AsyncBufReadExt as _, BufReader};
+use tokio::io::{self, AsyncBufReadExt as _, AsyncRead, BufReader};
 
 use crate::error::Result;
 
@@ -20,15 +20,23 @@ where
     let stdin = io::stdin();
 
     if stdin.as_fd().is_terminal() {
-        return None;
+        None
+    } else {
+        Some(try_read(stdin))
     }
+}
 
-    Some(try_stream! {
-        let mut stdin = BufReader::new(stdin);
+fn try_read<T>(reader: impl AsyncRead + Unpin) -> impl TryStream<Item = Result<T>>
+where
+    T: FromStr + 'static,
+    <T as FromStr>::Err: Error + Send + Sync,
+{
+    try_stream! {
+        let mut reader = BufReader::new(reader);
         let mut buf = String::new();
 
         loop {
-            let n = stdin.read_line(&mut buf).await?;
+            let n = reader.read_line(&mut buf).await?;
 
             if n == 0 {
                 break;
@@ -38,7 +46,7 @@ where
 
             buf.clear();
         }
-    })
+    }
 }
 
 #[inline]
@@ -47,4 +55,83 @@ where
     E: Into<Box<dyn Error + Send + Sync>>,
 {
     std::io::Error::new(std::io::ErrorKind::InvalidInput, e)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use futures_util::StreamExt as _;
+    use tokio_test::io::Builder;
+    use url::Url;
+
+    #[tokio::test]
+    async fn empty_input() {
+        let reader = Builder::new().build();
+        let input = try_read::<Url>(reader).collect::<Vec<_>>().await;
+
+        let expected: Vec<Url> = vec![];
+
+        match input.into_iter().collect::<Result<Vec<_>>>() {
+            Ok(actual) => assert_eq!(expected, actual),
+            Err(error) => panic!("expected read to succeed with no data, got error: {error:?}"),
+        }
+    }
+
+    #[tokio::test]
+    async fn valid_input() {
+        let reader = Builder::new()
+            .read(b"https://example")
+            .read(b".com\n")
+            .read(b"http://example.com\n")
+            .build();
+
+        let input = try_read::<Url>(reader).collect::<Vec<_>>().await;
+
+        let expected = vec![
+            Url::parse("https://example.com").expect("valid URL"),
+            Url::parse("http://example.com").expect("valid URL"),
+        ];
+
+        match input.into_iter().collect::<Result<Vec<_>>>() {
+            Ok(actual) => assert_eq!(expected, actual),
+            Err(error) => panic!("expected read to succeed with no data, got error: {error:?}"),
+        }
+    }
+
+    #[tokio::test]
+    async fn invalid_input() {
+        let reader = Builder::new()
+            .read(b"https://example.com\n")
+            .read(b"some invalid entry\n")
+            .build();
+
+        let input = try_read::<Url>(reader).collect::<Vec<_>>().await;
+        let input = input.into_iter().collect::<Result<Vec<_>>>();
+
+        if let Some(err) = input.expect_err("read should fail").source() {
+            assert_eq!(
+                err.downcast_ref::<std::io::Error>()
+                    .expect("expected an std::io::Error")
+                    .kind(),
+                std::io::ErrorKind::InvalidInput
+            );
+        }
+    }
+
+    #[tokio::test]
+    async fn io_error() {
+        let reader = Builder::new()
+            .read(b"https://example")
+            .read(b".com\n")
+            .read_error(std::io::Error::other("IO error"))
+            .build();
+
+        let input = try_read::<Url>(reader).collect::<Vec<_>>().await;
+        let input = input.into_iter().collect::<Result<Vec<_>>>();
+
+        if let Some(err) = input.expect_err("read should fail").source() {
+            assert!(err.to_string().contains("IO error"));
+        }
+    }
 }
